108158e2f4375d53f5b4614d7a01bcb68b10057c
harisaini778 <harikumarsaini778@gmail.com>
2024-02-01 16:16:40 +0530

    created mdodel for the expense table

--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -10,6 +10,16 @@ db.query(`
     email VARCHAR(255) NOT NULL,
     premium BOOLEAN DEFAULT false
   );
+
+  CREATE TABLE IF NOT EXISTS expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    category VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES myusers(id)
+  );
 `);
 
 module.exports = db;
